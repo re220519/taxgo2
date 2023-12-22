1fd201083614d8bed7d694da7058ec3f1f2ce6f5
Anas@bairuhaTech <anas@bairuhatech.com>
2023-12-22 09:49:38 +0530

    updated

--- src/component/feature/index.tsx
+++ src/component/feature/index.tsx
@@ -1,188 +1,239 @@
-import React from 'react'
-import './style.scss';
-import { Card } from 'antd';
+import React from "react";
+import "./style.scss";
+import { Card } from "antd";
 import first from "../../asset/first.png";
 import second from "../../asset/second.png";
 import third from "../../asset/third.png";
 import fourth from "../../asset/fourth.png";
 
-
 function Feature() {
   return (
-   <>
-   <div className='featurecontainer1'>
-       <div className='featurecontainer2'>
-         <div className='featureheader'> Our Features </div>
-         <div className='cardd'>
-            <div className='cardcontainer'>
-             
-            <Card className='card' 
-               cover={<img alt="example" src={first} />}>
-                <div className='text1'>
-                <div className='text3'>
-                <h1 className='text'>Global Access
- </h1></div>
-                   <div className='textt'>
-                 <p className='text2'>Cloud Based Accounting Software enables access to your and your client accounts anywhere and anytime.
- </p>
-                </div></div>
-                 
-            </Card>
-            <Card className='card'
-               cover={<img alt="example" src={second} />}>
-                <div className='text1'>
-                 <div className='text3'>
-                 <h1 className='text'> 
-                 Accounting Solution
-                 </h1></div>
-                 <div className='textt'>
-                 <p className='text2'>Tax GO Accounting Software manage invoices, customers, suppliers, banking and prepare reports on the GO. </p>
-                 </div></div>
-            </Card>
-            <Card className='card' 
-              cover={<img alt="example" src={third} />}>
-
-                 <div className='text1'>
-                 <div className='text3'>
-                 <h1 className='text'>General Consultancy </h1></div>
-                 <div className='textt'>
-                 <p className='text2'> 24/7 Consultancy support from Tax GO Accounting and software experts to help engage your business and save your time.</p>
-                 </div></div>
-            </Card>
-            <Card className='card' 
-               cover={<img alt="example" src={fourth} />}>
-               <div className='text1'>
-               <div className='text3'>
-                 <h1 className='text'>Regulated and Legislated </h1></div>
-                 <div className='textt'>
-                 <p className='text2'> Stay up to date with the latest legislation for tax, accounting, and payroll. Call us today to start managing your business.
-</p></div>
-</div>
-            </Card>
-           </div>
-           
-            <div className='cardcontainerr'>
-              <div className='cardcontainerrr'>
-           <Card className='cardm' 
-               cover={<img alt="example" src={first} />}>
-                <div className='text1m'>
-                <div className='text3m'>
-                <h1 className='textm'>Global Access
- </h1></div>
-                   <div className='texttm'>
-                 <p className='text2m'>Cloud Based Accounting Software enables access to your and your client accounts anywhere and anytime.
- </p>
-                </div></div>
-                 
-            </Card>
-            <Card className='cardm'
-               cover={<img alt="example" src={second} />}>
-                <div className='text1m'>
-                 <div className='text3m'>
-                 <h1 className='textm'> 
-                 Accounting Solution
-                 </h1></div>
-                 <div className='texttm'>
-                 <p className='text2m'>Tax GO Accounting Software manage invoices, customers, suppliers, banking and prepare reports on the GO. </p>
-                 </div></div>
-            </Card>
-            </div><br />
-            <div className='cardcontainerrr'>
-           <Card className='cardm' 
-               cover={<img alt="example" src={first} />}>
-                <div className='text1m'>
-                <div className='text3m'>
-                <h1 className='textm'>General Consultancy
- </h1></div>
-                   <div className='texttm'>
-                 <p className='text2m'>24/7 Consultancy support from Tax GO Accounting and software experts to help engage your business and save your time.
- </p>
-                </div></div>
-                 
-            </Card>
-            <Card className='cardm'
-               cover={<img alt="example" src={second} />}>
-                <div className='text1m'>
-                 <div className='text3m'>
-                 <h1 className='textm'> 
-                 Regulated and Legislated
-                 </h1></div>
-                 <div className='texttm'>
-                 <p className='text2m'>Stay up to date with the latest legislation for tax, accounting, and payroll. Call us today to start managing your business.
- </p>
-                 </div></div>
-            </Card>
+    <>
+      <div className="featurecontainer1">
+        <div className="featurecontainer2">
+          <div className="featureheader"> Our Features </div>
+          <div className="cardd">
+            <div className="cardcontainer">
+              <Card className="card" cover={<img alt="example" src={first} />}>
+                <div className="text1">
+                  <div className="text3">
+                    <h1 className="text">Global Access</h1>
+                  </div>
+                  <div className="textt">
+                    <p className="text2">
+                      Cloud Based Accounting Software enables access to your and
+                      your client accounts anywhere and anytime.
+                    </p>
+                  </div>
+                </div>
+              </Card>
+              <Card className="card" cover={<img alt="example" src={second} />}>
+                <div className="text1">
+                  <div className="text3">
+                    <h1 className="text">Accounting Solution</h1>
+                  </div>
+                  <div className="textt">
+                    <p className="text2">
+                      Tax GO Accounting Software manage invoices, customers,
+                      suppliers, banking and prepare reports on the GO.{" "}
+                    </p>
+                  </div>
+                </div>
+              </Card>
+              <Card className="card" cover={<img alt="example" src={third} />}>
+                <div className="text1">
+                  <div className="text3">
+                    <h1 className="text">General Consultancy </h1>
+                  </div>
+                  <div className="textt">
+                    <p className="text2">
+                      {" "}
+                      24/7 Consultancy support from Tax GO Accounting and
+                      software experts to help engage your business and save
+                      your time.
+                    </p>
+                  </div>
+                </div>
+              </Card>
+              <Card className="card" cover={<img alt="example" src={fourth} />}>
+                <div className="text1">
+                  <div className="text3">
+                    <h1 className="text">Regulated and Legislated </h1>
+                  </div>
+                  <div className="textt">
+                    <p className="text2">
+                      {" "}
+                      Stay up to date with the latest legislation for tax,
+                      accounting, and payroll. Call us today to start managing
+                      your business.
+                    </p>
+                  </div>
+                </div>
+              </Card>
             </div>
-            </div>
-            </div>
-           
-        <div className='featurebutton'>
-         <button className='featurebutton1'>Get Started</button>
-       </div>
-       </div>
-       
-   </div>
-   
-   <div className='mobcontainer'>
-  <div className='mobcontainer1'>
-    <div className='mobheader'>Our Features</div>
-    <div className='mobcard'>
-      <div className='mobcard1'>
-      <Card className='cardm1'
 
-              cover={<img alt="example" src={third} />}>
+            <div className="cardcontainerr">
+              <div className="cardcontainerrr">
+                <Card
+                  className="cardm"
+                  cover={<img alt="example" src={first} />}
+                >
+                  <div className="text1m">
+                    <div className="text3m">
+                      <h1 className="textm">Global Access</h1>
+                    </div>
+                    <div className="texttm">
+                      <p className="text2m">
+                        Cloud Based Accounting Software enables access to your
+                        and your client accounts anywhere and anytime.
+                      </p>
+                    </div>
+                  </div>
+                </Card>
+                <Card
+                  className="cardm"
+                  cover={<img alt="example" src={second} />}
+                >
+                  <div className="text1m">
+                    <div className="text3m">
+                      <h1 className="textm">Accounting Solution</h1>
+                    </div>
+                    <div className="texttm">
+                      <p className="text2m">
+                        Tax GO Accounting Software manage invoices, customers,
+                        suppliers, banking and prepare reports on the GO.{" "}
+                      </p>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+              <br />
+              <div className="cardcontainerrr">
+                <Card
+                  className="cardm"
+                  cover={<img alt="example" src={first} />}
+                >
+                  <div className="text1m">
+                    <div className="text3m">
+                      <h1 className="textm">General Consultancy</h1>
+                    </div>
+                    <div className="texttm">
+                      <p className="text2m">
+                        24/7 Consultancy support from Tax GO Accounting and
+                        software experts to help engage your business and save
+                        your time.
+                      </p>
+                    </div>
+                  </div>
+                </Card>
+                <Card
+                  className="cardm"
+                  cover={<img alt="example" src={second} />}
+                >
+                  <div className="text1m">
+                    <div className="text3m">
+                      <h1 className="textm">Regulated and Legislated</h1>
+                    </div>
+                    <div className="texttm">
+                      <p className="text2m">
+                        Stay up to date with the latest legislation for tax,
+                        accounting, and payroll. Call us today to start managing
+                        your business.
+                      </p>
+                    </div>
+                  </div>
+                </Card>
+              </div>
+            </div>
+          </div>
 
-                 <div className='text1t'>
-                 <div className='text3t'>
-                 <h1 className='texst'>General Consultancy </h1></div>
-                 <div className='texttt'>
-                 <p className='text2t'> 24/7 Consultancy support from Tax GO Accounting and software experts to help engage your business and save your time.</p>
-                 </div></div>
-            </Card>
-            <Card className='cardm1'
-               cover={<img alt="example" src={second} />}>
-                <div className='text1t'>
-                 <div className='text3t'>
-                 <h1 className='texst'> 
-                 Accounting Solution
-                 </h1></div>
-                 <div className='texttt'>
-                 <p className='text2t'>Tax GO Accounting Software manage invoices, customers, suppliers, banking and prepare reports on the GO. </p>
-                 </div></div>  
-            </Card>
-            <Card className='cardm1'
-               cover={<img alt="example" src={fourth} />}>
-                <div className='text1t'>
-                 <div className='text3t'>
-                 <h1 className='texst'> 
-                 Regulated and Legislated
-                 </h1></div>
-                 <div className='texttt'>
-                 <p className='text2t'>Stay up to date with the latest legislation for tax, accounting, and payroll. Call us today to start managing your business.
- </p>
-                 </div></div>
-            </Card>
-            <Card className='cardm1' 
-               cover={<img alt="example" src={first} />}>
-                <div className='text1t'>
-                <div className='text3t'>
-                <h1 className='texst'>Global Access
- </h1></div>
-                   <div className='texttt'>
-                 <p className='text2t'>Cloud Based Accounting Software enables access to your and your client accounts anywhere and anytime.
- </p>
-                </div></div>
-                 
-            </Card>
+          <div className="featurebutton">
+            <button className="featurebutton1">Get Started</button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className='featurebuton'>
-         <button className='featurebuton1'>Get Started</button>
-       </div>
-  </div>
-   </div>
-   </>
-  )
+
+      <div className="mobcontainer">
+        <div className="mobcontainer1">
+          <div className="mobheader">Our Features</div>
+          <div className="mobcard">
+            <div className="mobcard1">
+              <Card
+                className="cardm1"
+                cover={<img alt="example" src={third} />}
+              >
+                <div className="text1t">
+                  <div className="text3t">
+                    <h1 className="texst">General Consultancy </h1>
+                  </div>
+                  <div className="texttt">
+                    <p className="text2t">
+                      {" "}
+                      24/7 Consultancy support from Tax GO Accounting and
+                      software experts to help engage your business and save
+                      your time.
+                    </p>
+                  </div>
+                </div>
+              </Card>
+              <Card
+                className="cardm1"
+                cover={<img alt="example" src={second} />}
+              >
+                <div className="text1t">
+                  <div className="text3t">
+                    <h1 className="texst">Accounting Solution</h1>
+                  </div>
+                  <div className="texttt">
+                    <p className="text2t">
+                      Tax GO Accounting Software manage invoices, customers,
+                      suppliers, banking and prepare reports on the GO.{" "}
+                    </p>
+                  </div>
+                </div>
+              </Card>
+              <Card
+                className="cardm1"
+                cover={<img alt="example" src={fourth} />}
+              >
+                <div className="text1t">
+                  <div className="text3t">
+                    <h1 className="texst">Regulated and Legislated</h1>
+                  </div>
+                  <div className="texttt">
+                    <p className="text2t">
+                      Stay up to date with the latest legislation for tax,
+                      accounting, and payroll. Call us today to start managing
+                      your business.
+                    </p>
+                  </div>
+                </div>
+              </Card>
+              <Card
+                className="cardm1"
+                cover={<img alt="example" src={first} />}
+              >
+                <div className="text1t">
+                  <div className="text3t">
+                    <h1 className="texst">Global Access</h1>
+                  </div>
+                  <div className="texttt">
+                    <p className="text2t">
+                      Cloud Based Accounting Software enables access to your and
+                      your client accounts anywhere and anytime.
+                    </p>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </div>
+          <div className="featurebuton">
+            <button className="featurebuton1">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Feature;
--- src/component/navbar/index.tsx
+++ src/component/navbar/index.tsx
@@ -1,88 +1,183 @@
-import React ,{useState} from 'react'
-import './style.scss';
-import { Button ,Flex} from 'antd';
-import { PhoneOutlined }from '@ant-design/icons';
-import { MailOutlined }from '@ant-design/icons';
-import { FaFacebook, FaTwitter, FaInstagram, FaYoutube} from 'react-icons/fa';
+import React, { useState } from "react";
+import "./style.scss";
+import { Button, Flex } from "antd";
+import { PhoneOutlined } from "@ant-design/icons";
+import { MailOutlined } from "@ant-design/icons";
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 import { SiWinamp } from "react-icons/si";
-import {  Drawer } from 'antd';
-import { CloseCircleOutlined } from '@ant-design/icons';
-import logomobile from '../../asset/logomobile.png';
+import { Drawer } from "antd";
+import { CloseCircleOutlined } from "@ant-design/icons";
+import logomobile from "../../asset/logomobile.png";
 function Navbar() {
-    const [open, setOpen] = useState(false); 
-    
+  const [open, setOpen] = useState(false);
 
   const onClose = () => {
     setOpen(false);
   };
   return (
-   <>
-   <div className='Navbarcontainer'>
-    <div className='Button'>
-        <div className='button'>
-        <Flex gap="small"  className='button1'>   
-        <Button  icon={<PhoneOutlined style={{ color: 'white' ,backgroundColor: "green ", borderRadius: '50%',padding:'4px'}}/>}>Call Us</Button>
-        <Button icon={<MailOutlined className='mailicon'/>} >Mail Us</Button>
-        </Flex>
+    <>
+      <div className="Navbarcontainer">
+        <div className="Button">
+          <div className="button">
+            <Flex gap="small" className="button1">
+              <Button
+                icon={
+                  <PhoneOutlined
+                    style={{
+                      color: "white",
+                      backgroundColor: "green ",
+                      borderRadius: "50%",
+                      padding: "4px",
+                    }}
+                  />
+                }
+              >
+                Call Us
+              </Button>
+              <Button icon={<MailOutlined className="mailicon" />}>
+                Mail Us
+              </Button>
+            </Flex>
+          </div>
         </div>
-    </div>
-    <div className='Menu'>
-        <div className='Menu1'>
-           <div>HOME</div>
+        <div className="Menu">
+          <div className="Menu1">
+            <div>HOME</div>
             <div>SERVICES</div>
             <div>PRICING</div>
             <div>CONTACT</div>
             <div>SIGN IN</div>
             <div>REGISTER</div>
+          </div>
         </div>
-    </div>
-    <div className='Socialmedia'>
-        <div className='socialmedia'>
-            <div className='socialmedia1'>
-            <FaFacebook  size={20} style={{ margin: '0px 5px' ,backgroundColor: 'white', color: '#34B628' ,borderRadius: '50%' } } /> 
-            <FaInstagram size={20} style={{ margin: '0px 5px' , backgroundColor: 'white', color: '#34B628', borderRadius: '50%'}} /> 
-            <FaTwitter size={20} style={{ margin: '0px 5px' , backgroundColor: 'white',  color: '#34B628',borderRadius: '50%' }} />
-            <FaYoutube size={22} style={{ margin: '0px 5px' , backgroundColor: 'white',  color: '#34B628',borderRadius: '50%' }} />
+        <div className="Socialmedia">
+          <div className="socialmedia">
+            <div className="socialmedia1">
+              <FaFacebook
+                size={20}
+                style={{
+                  margin: "0px 5px",
+                  backgroundColor: "white",
+                  color: "#34B628",
+                  borderRadius: "50%",
+                }}
+              />
+              <FaInstagram
+                size={20}
+                style={{
+                  margin: "0px 5px",
+                  backgroundColor: "white",
+                  color: "#34B628",
+                  borderRadius: "50%",
+                }}
+              />
+              <FaTwitter
+                size={20}
+                style={{
+                  margin: "0px 5px",
+                  backgroundColor: "white",
+                  color: "#34B628",
+                  borderRadius: "50%",
+                }}
+              />
+              <FaYoutube
+                size={22}
+                style={{
+                  margin: "0px 5px",
+                  backgroundColor: "white",
+                  color: "#34B628",
+                  borderRadius: "50%",
+                }}
+              />
             </div>
-            <div className='english'>
-                <button className='en'>EN</button>
+            <div className="english">
+              <button className="en">EN</button>
             </div>
+          </div>
         </div>
-    </div>
-    <div className='mobile'><img  className="" src={logomobile} alt='' /></div>
-    <div className="simenu" onClick={()=>setOpen(true) }><SiWinamp  style={{ backgroundColor: 'yellow', borderRadius: '50%', padding: '4px', }}/> </div>
-    
-   </div>
-  
- <Drawer className="drawer"  placement="right" closable={false}  onClose={()=>setOpen(false)} open={open}
-footer={
-    <div style={{ textAlign: 'center' }}>
-      <CloseCircleOutlined style={{ fontSize: '24px', cursor: 'pointer',color: 'red',borderRadius:'50%' }} onClick={() => setOpen(false)} />
-    </div>
-  }
-  > 
-   
- <div className='menulist'> 
- <p>HOME</p>
- <p>SERVICES</p>
- <p>PRICING</p>
- <p>SIGN IN</p>
- <p>REGISTER</p>
- <p >CONTACT</p>
- 
- </div>
- <div className='socialmedias'>
-            <FaFacebook  size={20} style={{ margin: '0px 5px' ,backgroundColor: 'white', color: '#34B628' ,borderRadius: '50%' } } /> 
-            <FaInstagram size={20} style={{ margin: '0px 5px' , backgroundColor: 'white', color: '#34B628', borderRadius: '50%'}} /> 
-            <FaTwitter size={20} style={{ margin: '0px 5px' , backgroundColor: 'white',  color: '#34B628',borderRadius: '50%' }} />
-            <FaYoutube size={22} style={{ margin: '0px 5px' , backgroundColor: 'white',  color: '#34B628',borderRadius: '50%' }} />
-            </div>
-  </Drawer>
- 
- 
-      
-   </>
-  )
+        <div className="mobile">
+          <img className="" src={logomobile} alt="" />
+        </div>
+        <div className="simenu" onClick={() => setOpen(true)}>
+          <SiWinamp
+            style={{
+              backgroundColor: "yellow",
+              borderRadius: "50%",
+              padding: "4px",
+            }}
+          />{" "}
+        </div>
+      </div>
+
+      <Drawer
+        className="drawer"
+        placement="right"
+        closable={false}
+        onClose={() => setOpen(false)}
+        open={open}
+        footer={
+          <div style={{ textAlign: "center" }}>
+            <CloseCircleOutlined
+              style={{
+                fontSize: "24px",
+                cursor: "pointer",
+                color: "red",
+                borderRadius: "50%",
+              }}
+              onClick={() => setOpen(false)}
+            />
+          </div>
+        }
+      >
+        <div className="menulist">
+          <p>HOME</p>
+          <p>SERVICES</p>
+          <p>PRICING</p>
+          <p>SIGN IN</p>
+          <p>REGISTER</p>
+          <p>CONTACT</p>
+        </div>
+        <div className="socialmedias">
+          <FaFacebook
+            size={20}
+            style={{
+              margin: "0px 5px",
+              backgroundColor: "white",
+              color: "#34B628",
+              borderRadius: "50%",
+            }}
+          />
+          <FaInstagram
+            size={20}
+            style={{
+              margin: "0px 5px",
+              backgroundColor: "white",
+              color: "#34B628",
+              borderRadius: "50%",
+            }}
+          />
+          <FaTwitter
+            size={20}
+            style={{
+              margin: "0px 5px",
+              backgroundColor: "white",
+              color: "#34B628",
+              borderRadius: "50%",
+            }}
+          />
+          <FaYoutube
+            size={22}
+            style={{
+              margin: "0px 5px",
+              backgroundColor: "white",
+              color: "#34B628",
+              borderRadius: "50%",
+            }}
+          />
+        </div>
+      </Drawer>
+    </>
+  );
 }
 
 export default Navbar;
